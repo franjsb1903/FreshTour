c3b565d2fb4fe0e985eb62742952a3fdfba30e7c
franjsb1903 <franchu.besteiro@gmail.com>
2021-03-31 18:03:43 +0200

    Comezando creacion do subsistema planificador

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { StyleSheet, Platform, SafeAreaView, StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 
 import BottomTabNavigator from './components/BottomTabNavigator'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <BottomTabNavigator />
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <BottomTabNavigator />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+});
--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-const Map = () => {
-    return (
-        <View>
-            <Text>
-                Son a pantalla de mapa!
-            </Text>
-        </View>
-    )
-}
+//import leaflet_map from '../leaflet/leaflet_html.html'
+
+const leaflet_html = require("../leaflet/leaflet.html")
+
+class Map extends Component {
+    render() {
+        return (
+          <WebView
+            source={{ html: "../leaflet/leaflet.html" }}
+            style={{ flex: 1 }}
+          />
+        );
+      }
+  }
 
 export default Map;
